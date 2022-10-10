1eebe24ae9350fd20b9982e449a11cddeba46f35
Gotmished <5002@holbertonschool.com>
2022-10-10 04:22:58 -0700

    Attempting to fix isoformat

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -52,6 +52,6 @@ class BaseModel:
         new_dict = {}
         new_dict = self.__dict__
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
