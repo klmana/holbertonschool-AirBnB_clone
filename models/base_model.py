fce80cf1356d417d8618ff759a6c2f9ed01563aa
Gotmished <5002@holbertonschool.com>
2022-10-09 19:12:52 -0700

    Initial commit task three - create BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""
+Module containing "Base" class.
+"""
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    """
+    The base model class.
+    """
+    def __init__(self):
+        """
+        Constructs a BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str___(self):
+        """
+        Returns a string representation of BaseModel
+        """
+        return f"[self.__class__.__name__] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates public instance attribute updated_at
+        with current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__
+        """
+        new_dict = {}
+        new_dict = self.__dict__
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
