e8b16a8dc09d9c4124f631ed32203d1e888e4aed
root <chaubnphan@gmail.com>
2022-10-10 20:02:05 -0700

    Initial commit of console.py

--- console.py
+++ console.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
 """
-  Console
+  The Console program.
+
+
+  Contains the entry point of the command interpreter.
 """
+
+
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """Contains command prompts for HBNB program's intepreter."""
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """Quit command - exits the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command -  exits the program."""
+        return True
+
+    def empty_line(self):
+        """Empty line - executes nothing."""
+        return
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
