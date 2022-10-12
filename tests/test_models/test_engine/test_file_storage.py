8d0f9bd2aae3ac853ebc1572dcef57148150f27f
root <chaubnphan@gmail.com>
2022-10-11 17:57:30 -0700

    Update test_engine

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,13 +15,29 @@ import os
 FileStorage = file_storage.FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
-    """
-    Test Cases for the FileStorage class.
-    """
+class TestDocsFileStorage(unittest.TestCase):
+    """Tests for the FileStorage's docs."""
 
-    def setUp(self):
-        """
-        Sets up test methods.
-        """
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """Sets up the FileStorage class."""
+        cls.base_funcs = inspect.getmembers(FileStorage, inspect.isfunction)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions."""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+    def test_pycode_class(self):
+        """Checks pycodestyle for file storage."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pycode_test(self):
+        """Checks pycodestyle for test_file_storage."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_engine/test_file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
