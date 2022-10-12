845b4c99fe899e9c16f7e4b1c281ddb3f1c0f253
root <chaubnphan@gmail.com>
2022-10-11 17:34:50 -0700

    Initial commit of tests for city, place, review, state and user.

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Contains tests for User class."""
+
+import pycodestyle
+import unittest
+import datetime
+import uuid
+import inspect
+
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestDocsUser(unittest.Testcase):
+    """Tests for the User's docs."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Sets up the User class."""
+        cls.base_funcs = inspect.getmembers(User, inspect.isfunction)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions."""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+    def test_pycode_class(self):
+        """Checks pycodestyle for user."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pycode_test(self):
+        """Checks pycodestyle for test_user."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestUser(unittest.TestCase):
+    """Tests class functionality."""
+
+    def test_str(self):
+        """Tests that __str__ is functioning normally."""
+        self.assertEqual(self.__str__(), '[User]')
